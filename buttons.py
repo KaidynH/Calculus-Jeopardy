62151daa1c6ccb4a58ca9f39dca2416bb19d8ed7
KaidynH <kaidyn.haun@gmail.com>
2026-04-27 02:02:35 -0500

    Basis of calc jeopardy with buttons and click detection - already beutiified :)

---
+++ buttons.py
@@ -0,0 +1,65 @@
+import pygame, random
+import numpy as np
+
+unit_colors = {1:(120, 50, 235), 2:(235,50,190), 3:(10,160,215), 4:(235,245,76)}
+
+WIDTH = 1200
+HEIGHT = 800
+
+class button(pygame.sprite.Sprite):
+    def __init__(self, psection, units, sections, font):
+        # Initialize sprite class
+        super().__init__()
+
+        # Section
+        self.section_str = psection
+        self.section_float = float(psection)
+        self.section_int = int(self.section_float)
+
+        # Unit number and color
+        self.unit = 0
+        for i in range(1,5):
+            if self.section_str in units.get(i):
+                self.unit = i
+                break
+        self.color = unit_colors.get(self.unit)
+
+
+        # Coordinates and size of button
+        self.size = 35
+        x_border_size = 200
+        x_distance = int(WIDTH - x_border_size * 2) // (len(units)-1)
+        y_distance = int(self.size * 2.5)
+        y_border_size = int(HEIGHT/2 - (y_distance * (len(units.get(self.unit)) / 2) + 1))
+        self.centerx = x_border_size + ((self.unit-1) * x_distance)
+        self.centery = y_border_size+y_distance*.5 + np.where(units.get(self.unit) == self.section_str)[0][0] * y_distance
+        self.center = (self.centerx, self.centery)
+
+        # Collision rect
+        self.rect = pygame.Rect(0, 0, self.size*2, self.size*2)
+        self.rect.center = self.center
+
+        # Button text
+        self.text = font.render(self.section_str, True, "black") if self.section_float != 11.11 else font.render("11.10", True, "black")
+        self.text_rect = self.text.get_rect(center=self.center)
+
+        
+
+    def update(self, screen):
+        pygame.draw.circle(screen, (255,255,255), self.center, self.size+5)
+        pygame.draw.circle(screen, self.color, self.center, self.size)
+        screen.blit(self.text, self.text_rect)
+
+
+
+
+
+
+
+
+    
+
+
+        
+
+
---
+++ main.py
@@ -0,0 +1,67 @@
+import pygame, random
+import numpy as np
+from buttons import button
+
+# Pygame Setup
+WIDTH = 1200
+HEIGHT = 800
+pygame.init()
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+BG_COLOR = (70, 100, 70)
+SCREEN.fill(BG_COLOR)
+font = pygame.font.Font(None, 36)
+clock = pygame.time.Clock()
+FPS = 30
+
+
+# Sections Dicitionary and List
+sections_dic = {
+    7:np.array([f"7.{i}" for i in range(1,9)]),
+    8:np.array(["8.1", "8.2"]),
+    9:np.array(["9.1", "9.3", "9.5"]),
+    10:np.array([f"10.{i}" for i in range(1,5)]),
+    11:np.hstack([np.array([f"11.{i}" for i in range (1,7)]), np.array([f"11.{i}" for i in range (8,10)])])
+            }
+
+sections_dic.update({11:np.append(sections_dic.get(11), 11.11)})
+sections_ary = np.hstack([sections_dic.get(i) for i in range(7,12)])
+
+# Four Units
+units_dic = {
+    1:np.array([f"7.{i}" for i in range (1,8)]),
+    2:np.array(["7.8", "8.1", "8.2", "9.1", "9.3", "9.5", "10.1", "10.2"]),
+    3:np.array(["10.3", "10.4", "11.1", "11.2", "11.3", "11.4"]),
+    4:np.array(["11.5", "11.6", "11.8", "11.9", "11.11"])
+}
+
+# Buttons for all the questions
+buttons_grp = pygame.sprite.Group()
+for section in sections_ary:
+    buttons_grp.add(button(section, units_dic, sections_dic, font))
+
+# Main Runner
+running = True
+while running:
+    # Delay between frames then wipe screen before drawing
+    clock.tick(FPS)
+    SCREEN.fill(BG_COLOR)
+
+
+    # Handle Events
+    for event in pygame.event.get():
+        # Close the window
+        if event.type == pygame.QUIT:
+            running = False
+
+        # Check if button clicked
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            for button_sprt in buttons_grp:
+                if button_sprt.rect.collidepoint(event.pos):
+                    print(button_sprt.section_str)
+    
+
+    # Update pygame window
+    # pygame.draw.line(SCREEN, (0,255,0), (0, HEIGHT/2), (WIDTH, HEIGHT/2))
+    # pygame.draw.line(SCREEN, (0,255,0), (WIDTH/2, 0), (WIDTH/2, HEIGHT))
+    buttons_grp.update(SCREEN)
+    pygame.display.update()
